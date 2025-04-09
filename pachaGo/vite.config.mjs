f855f2163278945715315e3f7bf4b3b0360c383c
ailinbaudin <ailinbaudin@gmail.com>
2025-04-09 18:36:49 -0300

    edit vite for deploy

--- pachaGo/vite.config.mjs
+++ pachaGo/vite.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
         noDiscovery: true,
         include: ['mapbox-gl']
     },
-    base: process.env.VITE_BASE_PATH || '/', 
+    base: '/', 
     plugins: [
         vue(),
         Components({
